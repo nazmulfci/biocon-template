1b2399bf1de26ab0385eb1ffcf9f12492762032a
ShFarhan767 <farhan@doofazit.com>
2023-11-21 20:29:41 +0600

    done

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-jQuery("#carousel").owlCarousel({
-    autoplay: true,
-    rewind: true, /* use rewind if you don't want loop */
-    margin: 20,
-     /*
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    */
-    responsiveClass: true,
-    autoHeight: true,
-    autoplayTimeout: 7000,
-    smartSpeed: 800,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-  
-      600: {
-        items: 3
-      },
-  
-      1024: {
-        items: 4
-      },
-  
-      1366: {
-        items: 4
-      }
-    }
-  });
-
 const startBtn =document.querySelector("#startBtn"),
 endBtn =document.querySelector("#endBtn"),
 prevNext =document.querySelectorAll(".prevNext"),
